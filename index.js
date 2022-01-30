7f83078e76f1fb5ff6715c91f555f872e3b50082
Nisarga Adhikary <adhikarynisarga17@gmail.com>
2022-01-30 11:12:12 +0530

    OwO

---
+++ index.js
@@ -0,0 +1,26 @@
+require('dotenv').config();
+const Discord = require('discord.js');
+const axios = require('axios');
+const TOKEN = process.env.TOKEN;
+const bot = new Discord.Client();
+const targets = ['-waifu', '!waifu', '/waifu', '=waifu'];
+
+bot.login(TOKEN);
+
+bot.on('ready', () => {
+  console.info(`I'm ready, logged in as ${bot.user.tag}`);
+});
+
+bot.on('message', (msg) => {
+  if (msg.author.bot) return;
+
+  const foundWord = targets.find((target) => msg.content.includes(target));
+  if (foundWord) {
+    axios.get('https://api.waifu.pics/sfw/waifu').then((response) => {
+
+      msg.channel.send(
+        `Here you go ---> ${response.data.url}`
+      );
+    });
+  }
+});
